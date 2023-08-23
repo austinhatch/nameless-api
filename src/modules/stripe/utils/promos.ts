1cfc1ba3432c1a476241a210b21945cd9d494ebb
Jesse Jacob <jessejacob@Jesses-Air.fios-router.home>
2023-08-22 22:08:58 -0400

    jesse/fixing promos code

--- src/modules/stripe/utils/promos.ts
+++ src/modules/stripe/utils/promos.ts
@@ -1,11 +1,11 @@
-export const promos = {
-    'Organic Garmentz Pop Up':{
-        'INLYFE':{'percent':false, 'amount':10},
-        'INLYFE20':{'percent':true,'amount':25}
-        },
+export const promoCodes = {
+  'Organic Garmentz Pop Up': {
+    INLYFE: { percent: false, amount: 10 },
+    INLYFE20: { percent: true, amount: 25 },
+  },
 
-    'Under the Stars at Lillistar Rooftop':{
-        'NAMELESS':{'percent':false, 'amount':5},
-        'NAMELESS15':{'percent':true, 'amount':15}
-    }
-}
+  'Under the Stars at Lillistar Rooftop': {
+    NAMELESS: { percent: false, amount: 5 },
+    NAMELESS15: { percent: true, amount: 15 },
+  },
+};
--- src/modules/stripe/utils/stripe_config.ts
+++ src/modules/stripe/utils/stripe_config.ts
@@ -1,40 +1,35 @@
 import { environment } from '@/config/environment';
-const promoCodes = require('./promos.ts')
+import { promoCodes } from './promos';
 
-export const stripe = require("stripe")(
-  process.env.STRIPE_SK!,
-);
+export const stripe = require('stripe')(process.env.STRIPE_SK!);
 
-export function applyPromoCode(eventData:any, promoCode:string){
-  try{
-    console.log("Applying "+promoCode + " to " + eventData.name)
-    const promos = promoCodes.promos
-    console.log(promos)
-    if(eventData.name in promos){
-      if(promoCode in promos[eventData.name]){
-        const promo =  promos[eventData.name][promoCode]
+export function applyPromoCode(eventData: any, promoCode: string) {
+  try {
+    console.log('Applying ' + promoCode + ' to ' + eventData.name);
+    const promos = promoCodes.promos;
+    console.log(promos);
+    if (eventData.name in promos) {
+      if (promoCode in promos[eventData.name]) {
+        const promo = promos[eventData.name][promoCode];
         /** Reminder that Prices for stripe are x 100 so we dont need to handle %'s weirdly */
-        if(promo.percent){
-          const newPrice = eventData.priceUSD * (100-promo.amount)/100
-          if(newPrice > 0){
-            return newPrice
+        if (promo.percent) {
+          const newPrice = (eventData.priceUSD * (100 - promo.amount)) / 100;
+          if (newPrice > 0) {
+            return newPrice;
+          }
+        } else {
+          const newPrice = eventData.priceUSD - promo.amount;
+          if (newPrice > 0) {
+            return newPrice;
           }
         }
-        else{
-          const newPrice =eventData.priceUSD - promo.amount
-          if(newPrice > 0){
-            return newPrice
-          }        
-        }
-      }
-      else{
-        return null
+      } else {
+        return null;
       }
     }
-    return null
-  }
-  catch (e) {
-    console.log("Error applying promo code "+e)
-    return null
+    return null;
+  } catch (e) {
+    console.log('Error applying promo code ' + e);
+    return null;
   }
 }
