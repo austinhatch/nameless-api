5cffdab59579571acbad4ca4bdaa67e448dc507d
austinhatch <32887233+austinhatch@users.noreply.github.com>
2023-08-22 22:37:43 -0400

    Update promos.ts

--- src/modules/stripe/utils/promos.ts
+++ src/modules/stripe/utils/promos.ts
@@ -1,11 +1,13 @@
-export const promoCodes = {
-  'Organic Garmentz Pop Up': {
-    INLYFE: { percent: false, amount: 10 },
-    INLYFE20: { percent: true, amount: 25 },
-  },
+const promoCodes = {
+    'Organic Garmentz Pop Up':{
+        'INLYFE':{'percent':false, 'amount':10},
+        'INLYFE20':{'percent':true,'amount':25}
+        },
 
-  'Under the Stars at Lillistar Rooftop': {
-    NAMELESS: { percent: false, amount: 5 },
-    NAMELESS15: { percent: true, amount: 15 },
-  },
-};
+    'Under the Stars at Lillistar Rooftop':{
+        'NAMELESS':{'percent':false, 'amount':5},
+        'NAMELESS15':{'percent':true, 'amount':15}
+    }
+}
+
+export default promoCodes
