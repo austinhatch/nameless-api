0dc50bbac19c42efc92c96c86f4133a656f5a042
Oscar <jesserjacob99@gmail.com>
2023-08-28 00:08:06 -0400

    feat: dynamic products

--- src/modules/shopify/shopify.router.ts
+++ src/modules/shopify/shopify.router.ts
@@ -1,12 +1,11 @@
 import Router from '@koa/router';
-import { object, string, ref } from 'yup';
-import { validateRequestBodyMiddleware } from '@/shared/middlewares/validation/validate-request-body.middleware';
+// import { validateRequestBodyMiddleware } from '@/shared/middlewares/validation/validate-request-body.middleware';
 import { shopifyController } from './shopify.controller';
 
 export const shopifyRouter = new Router({ prefix: '/shopify' });
 
 shopifyRouter.get(
   '/get-all-products',
-//   validateRequestBodyMiddleware<ISignUpDTO>(signUpSchema.strict()),
+  //   validateRequestBodyMiddleware<ISignUpDTO>(signUpSchema.strict()),
   shopifyController.getAllProducts,
 );
