46e039f21ea2b46e51bf28d454803047e977d68d
austinhatch <ahatch@colgate.edu>
2023-09-04 11:12:42 -0400

    add fees to stripe

--- src/modules/stripe/stripe.controller.ts
+++ src/modules/stripe/stripe.controller.ts
@@ -2,7 +2,7 @@ import { RouterContext } from '@koa/router';
 import { ICreatePaymentIntentDTO } from './dtos/create_payment_intent';
 import { IApplyPromoCodeDTO } from './dtos/apply_promo_code';
 import { EventsRepository } from '../events/events.repository';
-import { stripe, applyPromoCode } from './utils/stripe_config';
+import { stripe, applyPromoCode, addFees } from './utils/stripe_config';
 
 export class stripeController {
 
@@ -10,7 +10,7 @@ export class stripeController {
         const { eventData } = <ICreatePaymentIntentDTO>JSON.parse(ctx.request.body)
         try {
             const paymentIntent = await stripe.paymentIntents.create({
-                amount: eventData.priceUSD*100,
+                amount: addFees(eventData.priceUSD*100),
                 currency: "usd",
                 automatic_payment_methods: {
                   enabled: true,
@@ -35,7 +35,7 @@ export class stripeController {
                 const metadata = {"promo_code": promoCode}
                 const paymentIntent = await stripe.paymentIntents.update(
                     id,
-                    {metadata: metadata, amount: updatedPrice*100}
+                    {metadata: metadata, amount: addFees(updatedPrice*100)}
                 )
                 ctx.status = 201
                 ctx.body = { id: paymentIntent.id, amount: paymentIntent.amount, clientSecret: paymentIntent.client_secret}
