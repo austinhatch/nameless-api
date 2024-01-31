25a3595c9dcfc9b006eb48ac4bb9300a1bbddcd6
austinhatch <ahatch@colgate.edu>
2024-01-30 19:29:17 -0500

    fix aptos pfp config and environment

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -17,7 +17,7 @@ export const environment = {
   },
   evm_private_key: process.env.EVM_PRIVATE_KEY!,
   aptos_private_key: process.env.APTOS_PRIVATE_KEY!,
-  aptos_public_key: process.env.APTOS_PBLIC_KEY!,
+  aptos_public_key: process.env.APTOS_PUBLIC_KEY!,
   aptos_chain: process.env.APTOS_CHAIN!,
   thirdweb: {
     secret: process.env.THIRDWEB_SECRET_KEY!,
