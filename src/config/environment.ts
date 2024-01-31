d482fadf1720c24f70c09cd8b352c47f75170fd2
oscar <jesserjacob99@gmail.com>
2024-01-30 19:18:28 -0500

    vars

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -18,6 +18,7 @@ export const environment = {
   evm_private_key: process.env.EVM_PRIVATE_KEY!,
   aptos_private_key: process.env.APTOS_PRIVATE_KEY!,
   aptos_public_key: process.env.APTOS_PBLIC_KEY!,
+  aptos_chain: process.env.APTOS_CHAIN!,
   thirdweb: {
     secret: process.env.THIRDWEB_SECRET_KEY!,
     client: process.env.THIRDWEB_CLIENT!,
--- src/modules/aptos/utils/aptos-config.ts
+++ src/modules/aptos/utils/aptos-config.ts
@@ -1,23 +1,28 @@
 import { environment } from '@/config/environment';
 import { Aptos, AptosConfig, Network, Ed25519PrivateKey, Account, AccountSequenceNumber  } from "@aptos-labs/ts-sdk";
 
-const aptosConfig = new AptosConfig({ network: Network.TESTNET }); // default to devnet
-export const aptos= new Aptos(aptosConfig);
-const privateKey = new Ed25519PrivateKey(environment.aptos_private_key);
-const aptosAccount = Account.fromPrivateKey({ privateKey });
-const accountSequenceNumber  = new AccountSequenceNumber(aptosConfig, aptosAccount, 100, 100, 100)
+let aptosConfig: AptosConfig;
 
-
-
-export function getAptosAccount(){
-    return aptosAccount
+if (environment.aptos_chain === 'TEST') {
+  aptosConfig = new AptosConfig({ network: Network.TESTNET });
+} else if (environment.aptos_chain === 'MAIN') {
+  aptosConfig = new AptosConfig({ network: Network.MAINNET });
+} else {
+  throw new Error('Invalid environment');
 }
 
+export const aptos = new Aptos(aptosConfig);
+const privateKey = new Ed25519PrivateKey(environment.aptos_private_key);
+const aptosAccount = Account.fromPrivateKey({ privateKey });
+const accountSequenceNumber = new AccountSequenceNumber(aptosConfig, aptosAccount, 1000, 1000, 1000);
+
+export function getAptosAccount() {
+  return aptosAccount;
+}
 
 export async function getSequenceNumber() {
-    const nextSequenceNumber = await accountSequenceNumber.nextSequenceNumber()
-    const nextSequenceNumberNumber = Number(nextSequenceNumber)
-     console.log(Number(nextSequenceNumber))
-    return nextSequenceNumberNumber
-
+  const nextSequenceNumber = await accountSequenceNumber.nextSequenceNumber();
+  const nextSequenceNumberNumber = Number(nextSequenceNumber);
+  console.log(Number(nextSequenceNumber));
+  return nextSequenceNumberNumber;
 }
